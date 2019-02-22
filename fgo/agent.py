aa0777db193cba32247e5ae7aff22da9aec1d53e
Chris Speck <cgspeck@gmail.com>
2019-02-22 13:10:36 +1100

    Friendly error message and make zero conf disablible

--- fgo/agent.py
+++ fgo/agent.py
@@ -15,22 +15,31 @@ from . import constants
 from .gql import schema
 
 class Agent():
-    _zeroconfThread = threading.Thread()
-    _mZeroconf = Zeroconf()
-    _zeroconfDesc = {'path': '/graphiql/', 'endpoint': '/graphql/'}
-
     def __init__(self, settings):
-        self._zeroconf_announce_interval = settings['zeroconf_announce_interval']
-        self._zeroconfInfo = ServiceInfo(constants.AGENT_SERVICE_TYPE,
-            settings['agent_service_name'],
-            socket.inet_aton(settings['my_ip']), constants.AGENT_PORT, 0, 0,
-            self._zeroconfDesc, f"{settings['my_hostname']}.local."
-        )
+
+        self._zeroconf_enabled = settings['zeroconf_enabled']
+
+        if self._zeroconf_enabled:
+            self._zeroconfThread = threading.Thread()
+            self._mZeroconf = Zeroconf()
+            self._zeroconfDesc = {'path': '/graphiql/', 'endpoint': '/graphql/'}
+            self._zeroconf_announce_interval = settings['zeroconf_announce_interval']
+            self._zeroconfInfo = ServiceInfo(constants.AGENT_SERVICE_TYPE,
+                settings['agent_service_name'],
+                socket.inet_aton(settings['my_ip']), constants.AGENT_PORT, 0, 0,
+                self._zeroconfDesc, f"{settings['my_hostname']}.local."
+            )
 
     def run(self):
         self._running = True
         app = self._create_app()
-        self._zeroconfAnnounce()
+
+        if self._zeroconf_enabled:
+            atexit.register(self._zeroconfUnregister)
+            logging.info("Registration of a service, press Ctrl-C to exit...")
+            self._mZeroconf.register_service(self._zeroconfInfo)
+            self._zeroconfAnnounce()
+
         app.run()
 
     def _zeroconfAnnounce(self):
@@ -49,10 +58,5 @@ class Agent():
 
     def _create_app(self):
         app = Flask(__name__)
-
-        atexit.register(self._zeroconfUnregister)
-        logging.info("Registration of a service, press Ctrl-C to exit...")
-        self._mZeroconf.register_service(self._zeroconfInfo)
         app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema.Schema, graphiql=True))
         return app
-
--- fgo/cli.py
+++ fgo/cli.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 import socket
+import sys
 import os
 
 from fgo import agent
@@ -13,6 +14,7 @@ def create_parser():
     parser.add_argument('--log-level', choices=log_levels, default=log_levels[0])
     parser.add_argument('--zeroconf-log-level', choices=log_levels, default=log_levels[0])
     parser.add_argument('--zeroconf-announce-interval', type=int, default=60)
+    parser.add_argument('--disable-zeroconf', action='store_true', default=False)
     parser.add_argument('--fqdn')
     parser.add_argument('--hostname')
     parser.add_argument('--ip')
@@ -24,31 +26,54 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     logging.basicConfig(level=getattr(logging, args.log_level))
-    logging.getLogger('zeroconf').setLevel(args.zeroconf_log_level)
     settings = {}
 
-    if args.fqdn:
-        settings['my_fqdn'] = args.fqdn
-    else:
-        logging.info('Finding your FQDN')
-        settings['my_fqdn'] = socket.getfqdn()
+    settings['zeroconf_enabled'] = not args.disable_zeroconf
 
-    if args.hostname:
-        settings['my_hostname'] = args.hostname
-    else:
-        logging.info('Finding your hostname')
-        settings['my_hostname'] = socket.gethostname()
+    if settings['zeroconf_enabled']:
+        logging.getLogger('zeroconf').setLevel(args.zeroconf_log_level)
 
-    if args.ip:
-        settings['my_ip'] = args.ip
-    else:
-        logging.info('Finding your IP Address')
-        settings['my_ip'] = os.getenv('IP_ADDRESS', socket.gethostbyname(settings['my_fqdn']))
-    
-    settings['zeroconf_announce_interval'] = args.zeroconf_announce_interval
+        if args.fqdn:
+            settings['my_fqdn'] = args.fqdn
+        else:
+            logging.info('Finding your FQDN')
+            settings['my_fqdn'] = socket.getfqdn()
 
-    settings['agent_service_name'] = f"FGO Agent ({settings['my_hostname']})._http._tcp.local."
-    logging.info(f"My Hostname: {settings['my_hostname']}, My FQDN: {settings['my_fqdn']}, My IP Address: {settings['my_ip']}")
+        if args.hostname:
+            settings['my_hostname'] = args.hostname
+        else:
+            logging.info('Finding your hostname')
+            settings['my_hostname'] = socket.gethostname()
+
+        if args.ip:
+            settings['my_ip'] = args.ip
+        else:
+            logging.info('Finding your IP Address')
+            try:
+                settings['my_ip'] = os.getenv('IP_ADDRESS', socket.gethostbyname(settings['my_fqdn']))
+            except socket.gaierror:
+                logging.error(f"""
+                    Unable to determine your IP Address! This usually happens when your fqdn is misidentified.
+
+                    Current settings: hostname={settings['my_hostname']} fqdn={settings['my_fqdn']}
+
+                    Run with --help to discover switches that you can use to hostname, fqdn, ip address etc.
+
+                    """)
+                sys.exit(1)
+
+        settings['zeroconf_announce_interval'] = args.zeroconf_announce_interval
+
+        settings['agent_service_name'] = f"FGO Agent ({settings['my_hostname']})._http._tcp.local."
+        logging.info(f"My Hostname: {settings['my_hostname']}, My FQDN: {settings['my_fqdn']}, My IP Address: {settings['my_ip']}")
+    else:
+        logging.info("Zeroconf is disabled")
+
     m_agent = agent.Agent(settings)
+
+    # work-around this [unfixed bug](https://github.com/pallets/flask/issues/1246#issuecomment-115690934)
+    if os.getenv('FLASK_ENV') == 'development':
+        os.environ['PYTHONPATH'] = os.getcwd()
+
     m_agent.run()
 
