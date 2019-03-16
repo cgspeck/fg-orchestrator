7b299b0bc863279521bec7503b064c2d144c5cf1
Chris Speck <cgspeck@gmail.com>
2019-03-16 20:42:36 +1100

    Allow user to specify a port, exit app properly, timer to check host statuses

--- fgo/director/registry.py
+++ fgo/director/registry.py
@@ -10,6 +10,7 @@ from fgo.director.signals import Signals
 @dataclass
 class RegisteredAgent:
     host: str
+    port: str = None
     online: bool = False
     uuid: str = None
     zeroconf_name: str = None
@@ -21,14 +22,17 @@ class Registry(QObject):
         self.signals = Signals()
         self._alive_agents: typing.Dict[str, RegisteredAgent] = {}
         self._dead_agents: typing.Dict[str, RegisteredAgent] = {}
-        self._unknown_agents: typing.List[str] = []
+        self._unknown_agents: typing.List[RegisteredAgent] = []
 
     def get_agents(self) -> typing.List[RegisteredAgent]:
-        return list(self._alive_agents.values()) + list(self._dead_agents.values()) + [RegisteredAgent(x) for x in self._unknown_agents]
+        return list(self._alive_agents.values()) + list(self._dead_agents.values()) + self._unknown_agents
+    
+    def check_agent_status(self):
+        logging.debug('Checking agent status')
 
-    @pyqtSlot(str, str, str)
-    def handle_zeroconf_agent_found(self, zeroconf_name: str, host: str, uuid: str):
-        logging.debug(f"handle_zeroconf_agent_found with name: {zeroconf_name}, host: {host}, uuid: {uuid}")
+    @pyqtSlot(str, str, str, str)
+    def handle_zeroconf_agent_found(self, zeroconf_name: str, host: str, port: str, uuid: str):
+        logging.info(f"handle_zeroconf_agent_found with name: {zeroconf_name}, host: {host}, port: {port}, uuid: {uuid}")
 
         # search by uuid, check if we have an existing dead agent, if so move it to the alive collection
         memo = [agent for agent in self._dead_agents.values() if agent.uuid == uuid]
@@ -42,15 +46,15 @@ class Registry(QObject):
             return
 
         # no uuid match, create a new agent
-        new_agent = RegisteredAgent(host, uuid=uuid, zeroconf_name=zeroconf_name, online=True)
+        new_agent = RegisteredAgent(host, port=port, uuid=uuid, zeroconf_name=zeroconf_name, online=True)
         self._alive_agents[uuid] = new_agent
 
         # check that we have a manually added agent with the same host name
         # TODO: make this a fuzzier test
-        memo = [agent for agent in self._unknown_agents if agent == host]
+        memo = [agent for agent in self._unknown_agents if agent.host == host]
         if memo:
             logging.debug(f"found agent matches an unknown agent")
-            self._unknown_agents.remove(host)
+            self._unknown_agents.remove(memo[0])
 
     @pyqtSlot(str)
     def handle_zeroconf_agent_removed(self, zeroconf_name: str):
@@ -70,19 +74,34 @@ class Registry(QObject):
     @pyqtSlot(str)
     def handle_agent_manually_added(self, host: str):
         logging.debug(f"handle_agent_manually_added with host: {host}")
+
+        frags = host.split(':')
+        port = '5000'
+
+        if len(frags) == 2:
+            host = frags[0]
+            port = frags[1]
         # only add unique hosts
-        if host not in [x.host for x in self.get_agents()]:
-            self._unknown_agents.append(host)
-        else:
+        if host in [x.host for x in self.get_agents()]:
             logging.debug(f"not adding duplicate host")
+            return
+
+        self._unknown_agents.append(
+            RegisteredAgent(
+                host=host,
+                port=port
+            )
+        )
+
 
     @pyqtSlot(str, str)
     def handle_agent_manually_removed(self, host: str, target_uuid: str = None):
         logging.debug(f"handle_agent_manually_removed with host: {host}, target_uuid: {target_uuid}")
         # check unknown list first
-        if host in self._unknown_agents:
-            logging.debug(f"removed host from unknown list")
-            self._unknown_agents.remove(host)
+        memo = [agent for agent in self._unknown_agents if agent.host == host]
+        if memo:
+            logging.debug(f"removed host {memo[0]} from unknown list")
+            self._unknown_agents.remove(memo[0])
 
         # check known lists
         if target_uuid:
