2c1cf93ab97b2026cf12d109e79afd6de2d5c645
Chris Speck <cgspeck@gmail.com>
2019-03-16 16:19:24 +1100

    Use a standard input dialog to allow a user to manually add a host

--- fgo/director/main.py
+++ fgo/director/main.py
@@ -1,6 +1,7 @@
 import atexit
+import sys
 
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QLineEdit
 from PyQt5.QtCore import QThreadPool
 
 from fgo.ui.MainWindow import Ui_MainWindow
@@ -15,17 +16,35 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self.show()
 
         self.registry = Registry()
         self.registry_model = RegistryModel(self, self.registry)
         self.tvAgents.setModel(self.registry_model)
+
+        self.actionExit.triggered.connect(lambda: sys.exit(0))
+        self.actionAddHost.triggered.connect(self.handle_add_host_triggered)
+        self.signals = Signals()
+        self.signals.agent_manually_added.connect(self.registry.handle_agent_manually_added)
+
+        self.show()
 
         listener = Listener()
         atexit.register(listener.stop)
         listener.signals.zeroconf_agent_found.connect(self.registry.handle_zeroconf_agent_found)
         listener.signals.zeroconf_agent_removed.connect(self.registry.handle_zeroconf_agent_removed)
         listener.run()
+
+    def handle_add_host_triggered(self):
+        text, okPressed = QInputDialog.getText(
+            self,
+            'Add host',
+            'Enter IP Address or hostname:',
+            QLineEdit.Normal,
+            ''
+        )
+
+        if okPressed and len(text.strip()) > 0:
+            self.signals.agent_manually_added.emit(text.strip())
 
 
 class DirectorRunner():
